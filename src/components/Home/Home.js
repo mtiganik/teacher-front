ccfb255b41d9e1f328c8abcfdbf1dd48f7080e39
Mihkel Tiganik <mtiganik@gmail.com>
2023-02-27 00:33:11 +0200

    Index page path modified

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Teacher from './Teacher'
-import { getEnv } from "./../env"
+import { getEnv } from "./../../env"
 
+const baseUrl = getEnv()["API_BASE_URL"]
 
 export function Home() {
     return (
       <div>
-        {console.log(getEnv())}
+        {console.log(baseUrl)}
         <h1>Hello, world!123</h1>
         <Teacher></Teacher>
       </div>
